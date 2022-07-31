6dfe1c6818d5622863a93e255d2bb0d4c00d42c0
Shamil Gumerov <shamusg12345@gmail.com>
2022-07-31 17:26:21 +0300

    Setup ansible on DHCP01. Establish ssh connectivity with SRV01

--- setup_DHCP01.sh
+++ setup_DHCP01.sh
@@ -25,6 +25,9 @@ callbacks_enabled = ansible.posix.profile_tasks, ansible.posix.timer
 EOF
 
 
+# create and inject ssh key for ansible to SRV01. Assume 'ansible' user @ 192.168.56.103 exists
+ssh-keygen -t rsa -b 4096 -N '' -q -f ~/.ssh/ansible_id_rsa
+ssh-copy-id -i ~/.ssh/ansible_id_rsa.pub -o StrictHostKeyChecking=no ansible@192.168.56.103 
 
 
 
