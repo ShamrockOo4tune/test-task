7e84b4b1af9ab378241017b159f3f5defcb37ccb
Shamil Gumerov <shamusg12345@gmail.com>
2022-08-01 17:28:34 +0300

    role install_software done

--- setup_DHCP01.sh
+++ setup_DHCP01.sh
@@ -9,26 +9,27 @@ nmcli con up enp0s3
 sudo yum -y install epel-release wget curl git tree 
 sudo yum -y install ansible
 
+# add ansible-playbook executable shortcut
+sudo ln -s $(which ansible-playbook) /usr/bin/ansible-playbook || true
+
 # Create ansible project directory structure
 sudo ansible-galaxy init --init-path /ansible install_software
 sudo ansible-galaxy init --init-path /ansible configure_server
 sudo chown $USER -R /ansible
 tree -d /ansible
 
-# tweak ansible config
-cat <<EOF > /ansible/ansible.cfg
+# Logs directory and access
+sudo mkdir -p /var/log/ansible
+sudo chown $USER /var/log/ansible
+
+# Tweak ansible config
+cat <<EOF > ~/.ansible.cfg
 [defaults]
-log_path          = /ansible/project_logs.log
-inventory         = ./hosts
-stdout_callback   = community.general.yaml
+log_path          = /var/log/project_logs.log
+stdout_callback   = yaml
 callbacks_enabled = ansible.posix.profile_tasks, ansible.posix.timer 
 EOF
 
-
-# create and inject ssh key for ansible to SRV01. Assume 'ansible' user @ 192.168.56.103 exists
+# create and inject ssh key to SRV01. Assume 'ansible' user @ 192.168.56.103 exists
 ssh-keygen -t rsa -b 4096 -N '' -q -f ~/.ssh/ansible_id_rsa
 ssh-copy-id -i ~/.ssh/ansible_id_rsa.pub -o StrictHostKeyChecking=no ansible@192.168.56.103 
-
-
-
-
